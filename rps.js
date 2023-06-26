5f4a7711cd57eb2623e8f3c2a4cc979eb58354a1
Steven Huang <96713218+huangs-nyc@users.noreply.github.com>
2023-06-26 10:59:35 -0400

    updates

--- rps.js
+++ rps.js
@@ -1,6 +1,56 @@
+let startGame = false
+let rockEl = document.querySelector("#rock-btn")
+let paperEl = document.querySelector("#paper-btn")
+let scissorsEl = document.querySelector("#scissors-btn")
+let playerChoice = ""
 
+// initially disable all buttons
+disableBtns()
 
-let player = {
-    
-
+function startGame() {
+    disableBtns()
 }
+
+// function startGame() {
+//     startGame = true;
+//     rockEl.textContent = "test1"
+// }
+
+// // function for computer to make a choice
+// function computerChoice() {
+//     let computerChoice = Math.floor(Math.random()*3)
+//     if (computerChoice === 0) {
+//         return "rock"
+//     } else if (computerChoice === 1) {
+//         return "paper"
+//     } else {
+//         return "scissors"
+//     }
+// }
+
+// // function for player to make a choice
+// function playerChoice() {
+//     rockEl.addEventListener("click", playerChoiceHelper("rock"))
+//     paperEl.addEventListener("click", playerChoiceHelper("paper"))
+//     scissorsEl.addEventListener("click", playerChoiceHelper("scissors"))
+// }
+
+// // helper function for player choices
+// function playerChoiceHelper(clickedChoice) {
+//     disableBtns()
+//     return clickedChoice
+// }
+
+// function to disable buttons
+function disableBtns() {
+    rockEl.setAttribute('disabled', 'true')
+    paperEl.disabled = true;
+    scissorsEl.disabled = true;
+}
+
+// // function to enable buttons
+// function enableBtns() {
+//     rockEl.disabled = false;
+//     paperEl.disabled = false;
+//     scissorsEl.disabled = false;
+// }
